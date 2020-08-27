73246e1467d277f587d8e8adadf14534c95fd44b
Simon Rieß <riess.simon@yahoo.de>
2020-08-27 20:39:50 +0200

    Update Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae86b2b916d5145679ee8c3a47586277-1464248984.eu-central-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
